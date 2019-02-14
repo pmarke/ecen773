e283939f95bb5aa519142514e0c8b66dcd8d8e1b
Mark Petersen <markiepetersen@gmail.com>
2019-02-14 11:06:04 -0700

    finished hw 2

--- 2_hw/Prob8_4.m
+++ 2_hw/Prob8_4.m
@@ -1,24 +1,11 @@
 %% Problem 8_4
 
 
-% b) Compute the matrix A(t) that corresponds to the given state transition
-% matrix
+%%% a) Compute the state transition matrix phi(t,t0)
+
 syms t t0
 
-PHI = [exp(t-t0)*cos(2*(t-t0)) exp(-2*(t-t0))*sin(2*(t-t0));...
-       -exp(t-t0)*sin(2*(t-t0)) exp(-2*(t-t0))*cos(2*(t-t0))];
 
-% Phi_dot = A(t)*PHI
-PHI_dot = diff(PHI,t);
-
-A = PHI_dot*inv(PHI);
-simplify(A)
-
-% c) Compute the eigen values of A(t)
-lambda = eig(A)
-
-
-   
 PHI1 = [exp(t)*cos(2*(t)) exp(-2*(t))*sin(2*(t));...
        -exp(t)*sin(2*(t)) exp(-2*(t))*cos(2*(t))];
    
@@ -30,6 +17,35 @@ PHI2 = [exp(t0)*cos(2*(t0)) exp(-2*(t0))*sin(2*(t0));...
 
 % Property 5.4 states
 % PHI(t,tau)^-1 = PHI(tau,t)
+
+
+PHI = PHI1*inv(PHI2);
+PHI = simplify(PHI)
+
+%%% b) Compute the matrix A(t) that corresponds to the given state transition matrix
+
+% Phi_dot = A(t)*PHI
+PHI_dot = diff(PHI,t);
+
+A = PHI_dot*inv(PHI);
+A = simplify(A)
+
+%%% c) Compute the eigen values of A(t)
+lambda = eig(A);
+lambda = simplify(lambda)
+
+%%% d) Classify this system in terms of Lyapunov stability
+% We can look at the stability of the sytem by analyzing the
+% the state transitioin matrix to see if it become arbitrarily large.
+% The sin and cos functions are bounded between [-1,1], and the exponential
+% functions are bounded between [0,0) depending on the value of t. Thus
+% we can analyze the stability of PHI by looking at is as t -> infinity. 
+% By inspection, we
+% can see that ||PHI|| -> inf and t -> inf. Thus the system is not stable.
+
+parmas.m
+
+
    
-temp = PHI1*inv(PHI2);
-simplify(temp)
+
+   
--- 2_hw/aircraft/parmas.m
+++ 2_hw/aircraft/parmas.m
@@ -2,20 +2,22 @@
 % x_dot = A*x + B*u
 
 P.A = [-0.038   18.984   0      -32.174;...
-     -0.001  -0.632    1       0;...
-     0       -0.759   -0.518   0;...
-     0        0        1       0];
+       -0.001  -0.632    1       0;...
+        0       -0.759   -0.518   0;...
+        0        0        1       0];
 
 P.B =   [10.1       0;...
-      0        -0.0086;...
-      0.025    -0.011;...
-      0         0];
+          0        -0.0086;...
+          0.025    -0.011;...
+          0         0];
   
-%% a. Compute the modes of this system
+%%% a. 
+% Compute the modes of this system
 % The mode is defined as exp(lambda_i*t)Vi
 [V,lambda] = eig(P.A);
 
-%% b. Analyze these modes physically by looking at how the physical states play a role in each mode. 
+%%% b. 
+% Analyze these modes physically by looking at how the physical states play a role in each mode. 
 % Are there some modes that deal more with the pitch? Others that have more
 % effect on the angle of attack?
 V_r = [0.9953    0.9953    1.0000    1.0000;...
@@ -27,38 +29,83 @@ lambda_r = [-0.5825 ;...
             -0.5825 ;...
             -0.0115 ;...
             -0.0115];...
+%%
+% We can analyze these modes physically by looking at their real parts. If 
+% the initial state of the aircraft is along the span of any of thses
+% modes, then the states of the aircraft will evolve as X(t) = sigma
+% exp(lambda_i*t)X(0) where X(0) = sigma*V_i and sigma is a scalar. By
+% looking at the eigen vector associated with the mode, we can see how
+% some physical states are affected vs other physical states. In all of
+% the modes, velocity is affected the most. The first two modes seem to have 
+% more affect on the pitch and and angle of attack than the other two
+% modes.
 
-%  We can analyze these modes physically by looking at their real parts. If 
-%  the initial state of the aircraft is along the span of any of thses
-%  modes, then the states of the aircraft will evolve as X(t) = sigma
-%  exp(lambda_i*t)X(0) where X(0) = sigma*V_i and sigma is a scalar. By
-%  looking at the eigen vector associated with the mode, we can see how
-%  some physical states are affected vs other physical states. In all of
-%  the modes, velocity is affected the most. The first two modes seem to have 
-%  more affect on the pitch and and angle of attack than the other two
-%  modes.
-
-%% C. Aircraft generally exhibit two longitudinal motions (also called modes),
+%%% c. 
+% Aircraft generally exhibit two longitudinal motions (also called modes),
 % a phugoid and short period mode. Phugoid represents the coupling between
 % the vehicle altitude and the airspeed, while the short period mode (whith
 % much faster dynamics) is the coupling between the angle of attack and the
 % pitch rate. Identify the values from (a) that best represent the phugoid
 % and short period modes respectively. 
 
-%  The first two modes show a strong coupling between the angle of attack
-%  and the pitch rate, these must be the short period modes. By default the
-%  other two modes must be the phugoid modes. ?????????????????
+%%
+% The first two modes show a strong coupling between the angle of attack
+% and the pitch rate, and they have faster dynamics since the eigen values
+% are larger, these must be the short period modes. By default the
+% other two modes must be the phugoid modes. 
 
-%% D. Given an input this aircraft wil respond along a linear combination 
+%%% d. 
+% Given an input this aircraft wil respond along a linear combination 
 % of the modes. Determine how strongly each of the inputs will effect each
 % of the different modes. Will one input effect one physical parameter more
 % strongly? 
 
-        
-% All of the real parts of the eigen values of A are negative, thus
-% the system is exponentially stable.
+W = inv(V);
 
+% Throttle along mode one
+th_v1 = W(1,:)*P.B(:,1)
 
+% Throttle along mode two
+th_v2 = W(2,:)*P.B(:,1)
+
+% Throttle along mode three
+th_v3 = W(3,:)*P.B(:,1)
+
+% Throttle along mode four
+th_v4 = W(4,:)*P.B(:,1)
+
+% Elevator along mode one
+el_v1 = W(1,:)*P.B(:,2)
+
+% Elevator along mode two
+el_v2 = W(2,:)*P.B(:,2)
+
+% Elevator along mode three
+el_v3 = W(3,:)*P.B(:,2)
+
+% Elevator along mode four
+el_v4 = W(4,:)*P.B(:,2)
+
+%%
+% Throttle affects modes 3 and 4 the most, and the elevator commands
+% affects all the modes about equally in magnitude. This makes sense since
+% modes 3 and 4 are more for air speed and throttle should affect the air
+% speed most. 
+
+%% TEST
+
+% syms c11 c12 c13 c21 c22 c23 c31 c32 c33
+% syms v11 v12 v13 v21 v22 v23 v31 v32 v33
+% syms e1 e2 e3
+% syms x1 x2 x3
+% C =[c11 c12 c13; c21 c22 c23; c31 c32 c33];
+% V = [v11 v12 v13; v21 v22 v23; v31 v32 v33];
+% E = [e1 0 0; 0 e2 0; 0 0 e3];
+% X = [x1;x2;x3];
+% 
+% C*E*V*X
+
+% W = inv(P.V)
 
       
  
