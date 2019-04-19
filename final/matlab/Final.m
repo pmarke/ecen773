2e4a663945271bf002f3aa6878d6722a7783daee
Mark Petersen <markiepetersen@gmail.com>
2019-04-19 17:13:09 -0600

    finished final

--- final/matlab/Final.m
+++ final/matlab/Final.m
@@ -1,4 +1,4 @@
-%%Question 1:
+%% Question 1:
 
 % System 1 and 2
 A = [0 1; 1 0];
@@ -14,16 +14,16 @@ rank(ctrb(A,B2))
 rank(obsv(A,C))
 
 %% Question 3
-%%% a)
+%%%% a)
 A = [-2 4; 1 1];
-b = [1;1];
+B = [1;1];
 C = [1 0];
 eig(A);
 
 syms s
 C*inv(s*eye(2)-A)*B;
 
-%%% b)
+%%%% b)
 syms t to
 PHI = [1 0; 1/2*(t^2-to^2) 1];
 A = diff(PHI,t)*inv(PHI)
